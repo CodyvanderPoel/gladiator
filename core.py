cc2a7db362280b9aae69b93b5eb0944068d245d1
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-07-09 15:15:00 -0500

    The both of these files are entirely complete and I am fixing to work on the shell

--- core.py
+++ core.py
@@ -2,6 +2,9 @@ from random import randint
 
 
 def new_gladiator(health, rage, damage_low, damage_high):
+    '''Preconditions : Health must be in between 0 and 100!
+                       Rage must be in between 0 and 100!
+    '''
     gladiator = {
         'Health': health,
         'Rage': rage,
@@ -12,24 +15,24 @@ def new_gladiator(health, rage, damage_low, damage_high):
 
 
 def attack(attacker, defender):
-    crit = randint(0,100)
-    attack = attacker.randint(['Low Attack'],['High Attack'])
-    if crit is <= attacker['Rage']:
+    crit = randint(0, 100)
+    attack = randint(attacker['Low Attack'], attacker['High Attack'])
+    if crit <= attacker['Rage']:
         attack = attack * 2
         attacker['Rage'] = attacker['Rage'] * 0
     else:
-        attacker['Rage']  = attacker['Rage'] + 15
+        attacker['Rage'] = attacker['Rage'] + 15
     defender['Health'] = defender['Health'] - attack
-
-
+    return defender['Health']
 
 
 def heal(gladiator):
     if gladiator['Rage'] > 10 and gladiator['Health'] <= 95:
         gladiator['Rage'] = gladiator['Rage'] - 10
         gladiator['Health'] = gladiator['Health'] + 5
+        return gladiator['Health']
     else:
-        print('No can do amigo!')
+        return None
 
 
 def is_dead(gladiator):
--- test_core.py
+++ test_core.py
@@ -1,7 +1,97 @@
 from core import *
 
 
-def test_new_gladiator(output):
-    result = new_gladiator()
+def test_new_gladiator():
+    result = new_gladiator(75, 40, 15, 55)
 
-    assert result
+    assert result == {
+        'Health': 75,
+        'Rage': 40,
+        'Low Attack': 15,
+        'High Attack': 55
+    }
+
+
+def test_new_gladiator_2():
+    result = new_gladiator(100, 40, 1, 10)
+
+    assert result == {
+        'Health': 100,
+        'Rage': 40,
+        'Low Attack': 1,
+        'High Attack': 10
+    }
+
+
+def test_new_gladiator_3():
+    result = new_gladiator(0, 0, 0, 0)
+
+    assert result == {
+        'Health': 0,
+        'Rage': 0,
+        'Low Attack': 0,
+        'High Attack': 0
+    }
+
+
+def test_attack():
+    gladiator_a = new_gladiator(100, 15, 20, 50)
+    gladiator_b = new_gladiator(80, 30, 15, 60)
+    result = attack(gladiator_a, gladiator_b)
+    assert result == gladiator_b['Health']
+
+
+def test_attack_2():
+    gladiator_a = new_gladiator(75, 55, 40, 70)
+    gladiator_b = new_gladiator(100, 10, 10, 30)
+    result = attack(gladiator_a, gladiator_b)
+    assert result == gladiator_b['Health']
+
+
+def test_attack_3():
+    gladiator_a = new_gladiator(0, 0, 0, 0)
+    gladiator_b = new_gladiator(0, 0, 0, 0)
+    result = attack(gladiator_a, gladiator_b)
+    assert result == gladiator_b['Health']
+
+
+def test_heal():
+    gladiator_a = new_gladiator(100, 15, 20, 50)
+    result = heal(gladiator_a)
+    assert result == None
+
+
+def test_heal_2():
+    gladiator_a = new_gladiator(95, 15, 20, 50)
+    result = heal(gladiator_a)
+    assert result == gladiator_a['Health']
+
+
+def test_heal_3():
+    gladiator_a = new_gladiator(75, 55, 20, 50)
+    result = heal(gladiator_a)
+    assert result == gladiator_a['Health']
+
+
+def test_heal_4():
+    gladiator_a = new_gladiator(80, 5, 20, 50)
+    result = heal(gladiator_a)
+    assert result == None
+
+
+def test_is_dead():
+    gladiator_a = new_gladiator(0, 30, 20, 50)
+    result = is_dead(gladiator_a)
+    assert result == True
+
+
+def test_is_dead_2():
+    gladiator_a = new_gladiator(100, 30, 20, 50)
+    result = is_dead(gladiator_a)
+    assert result == None
+
+
+def test_is_dead_3():
+    gladiator_a = new_gladiator(10, 30, 20, 50)
+    result = is_dead(gladiator_a)
+    assert result == None
