443342d3a273a2e7cc6ee0afb0b6185a086b0b66
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-07-09 16:12:40 -0500

    I have worked on the shell and made some work into it

---
+++ shell.py
@@ -0,0 +1,26 @@
+from core import *
+
+
+def get_name():
+    name = input('Combatant One! What is your name?')
+    print('WELCOME TO THE ARENA', name.upper(), '!')
+    return name
+
+
+def get_name_2():
+    name = input('Combatant Two! What is your name?')
+    print('WELCOME TO THE ARENA', name.upper(), '!')
+    return name
+def combatant_1_turn():
+    
+
+
+def main():
+    name_a = get_name()
+    user_a = new_gladiator(100, 15, 20, 40)
+    name_b = get_name_2()
+    user_b = new_gladiator(75, 30, 25, 45)
+
+
+if __name__ == '__main__':
+    main()
